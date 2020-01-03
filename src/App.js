6a1b359bd5bc8feef938e9c0c711efa459c0e200
VovaSidorov <VovaCivic@gmail.com>
2020-01-03 16:54:38 +0200

    Try redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    console.log(this.props.testStore);
+    return (
+    <div>
+      <input type="text"/>
+      <button>Add fdgfdgfdgdf</button>
+      <ul>
+        {this.props.testStore.map((track,index)=>
+        <li key={index}>{track}</li>
+          )}
+      </ul>
     </div>
   );
 }
+}
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = [
+    'Smells like spirit',
+    'Enter Sandman'
+  ];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function playlist(state = initialState, action) {
+    if (action.type === 'ADD_TRACK') {
+      return [
+        ...state,
+        action.payload
+      ];
+    }
+    return state;
+  }
+  
+  const store = createStore(playlist);
+
+    ReactDOM.render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+        document.getElementById('root')
+      );
+
